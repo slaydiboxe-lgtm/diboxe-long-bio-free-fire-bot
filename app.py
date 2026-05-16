896e587bc20054bdf48c75ae3104bcf89f283b75
slaydiboxe-lgtm <slaydiboxe@gmail.com>
2026-05-16 21:57:52 +0545

    Update API URL for bio update endpoint

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from aiohttp import web
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-API_URL = "ff-bio-api-production.up.railway.app/update_bio"
+API_URL = "http://np2.npcloud.online:2053/update_bio"
 PORT = int(os.getenv("PORT", 5000))
 
 intents = discord.Intents.default()
